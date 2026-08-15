b247462009a459b96bce31377b894db14261df60
Park, Yeonji <yeonpyj23@gmail.com>
2026-08-15 23:31:42 +0900

    chore: S3Client 빈과 aws 설정 프로퍼티 추가 (#13)

---
+++ src/main/java/com/github/galpiii/galpi/global/config/S3Config.java
@@ -0,0 +1,19 @@
+package com.github.galpiii.galpi.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Bean
+    public S3Client s3Client(AwsS3Properties properties) {
+        return S3Client.builder()
+                .region(Region.of(properties.region()))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
